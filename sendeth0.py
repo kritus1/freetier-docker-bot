965604e84fed1543cf347948d28e2aca0d7d1432
kritus1 <135320264+kritus1@users.noreply.github.com>
2023-06-09 13:52:47 +0300

    Update sendeth0.py

--- sendeth0.py
+++ sendeth0.py
@@ -3,5 +3,5 @@ import requests
 import eth0
 
 text = eth0.reply
-x = requests.post('https://api.telegram.org/bot6220437234:AAHPzY6Ajdy0j6WHmpuNmLBV8Wa-Q-3f0HQ/sendMessage?chat_id=1358508532&text='+str(text))
+x = requests.post('https://api.telegram.org/bot[TOKEN]?chat_id=[chatID]&text='+str(text))
 print(x.text)
